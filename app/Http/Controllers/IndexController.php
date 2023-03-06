598aac38703c7a9d7844ba2f138bc52a94323129
Kostya <koooooostaaa@gmail.com>
2023-03-06 15:19:06 +0200

    add inertia middleware, add home view

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class IndexController extends Controller
+{
+    public function index(): Response
+    {
+        return Inertia::render('Home', [
+            'title' => 'my_title'
+        ]);
+    }
+}
